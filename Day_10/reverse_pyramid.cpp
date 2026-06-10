f34181c47bfae00c9899aa5f38d64cad6e3ab1c1
itishagupta22 <itishagupta22@gmail.com>
2026-06-10 13:05:15 +0530

    Added reverse pyramid program

---
+++ Day_10/reverse_pyramid.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, i, j;
+    cout << "Enter the desired number of rows: ";
+    cin >> n;
+
+    for(i = 1; i <= n; i++)
+    {
+        for(j = 1; j <= i - 1; j++)
+        {
+            cout << " ";
+        }
+        for(j = 1; j <= 2 * (n - i) + 1; j++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
